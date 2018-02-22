f6847a44478d17fca809b755986f63e1e809aa18
Sven Rhinow <sven@sr-tag.de>
2018-02-22 13:30:47 +0100

    + InsertTagListener

---
+++ src/EventListener/InsertTagsListener.php
@@ -0,0 +1,96 @@
+<?php
+
+/*
+ * This file is part of project-manager-bundle.
+ *
+ * Copyright (c) 2004-2018 Sven Rhinow
+ *
+ * @license LGPL-3.0+
+ */
+
+namespace Srhinow\ProjectManagerBundle\EventListener;
+
+use Contao\CoreBundle\Framework\ContaoFrameworkInterface;
+//use Srhinow\ThemeSectionArticleModel;
+//use Srhinow\ModuleThemeArticle;
+
+/**
+ * Handles insert tags for news.
+ *
+ * @author Andreas Schempp <https://github.com/aschempp>
+ */
+class InsertTagsListener
+{
+    /**
+     * @var ContaoFrameworkInterface
+     */
+    private $framework;
+
+    /**
+     * @var array
+     */
+    private $supportedTags = [
+//        'insert_theme_article',
+//        'insert_theme_content',
+    ];
+
+    /**
+     * Constructor.
+     *
+     * @param ContaoFrameworkInterface $framework
+     */
+    public function __construct(ContaoFrameworkInterface $framework)
+    {
+        $this->framework = $framework;
+    }
+
+    /**
+     * Replaces news insert tags.
+     *
+     * @param string $tag
+     *
+     * @return string|false
+     */
+//    public function onReplaceInsertTags($tag)
+//    {
+//        $elements = explode('::', $tag);
+//        $key = strtolower($elements[0]);
+//
+//        if (\in_array($key, $this->supportedTags, true)) {
+//            return $this->replaceThemeArticleInsertTags($key, $elements[1]);
+//        }
+//
+//        return false;
+//    }
+
+
+    /**
+     * Replaces a THEME-ARTICLE-related insert tag.
+     *
+     * @param string $insertTag
+     * @param string $idOrAlias
+     *
+     * @return string
+     */
+//    private function replaceThemeArticleInsertTags($insertTag, $idOrAlias)
+//    {
+//        $this->framework->initialize();
+//
+//        /** @var ThemeSectionArticleModel $adapter */
+//        $adapter = $this->framework->getAdapter(ThemeSectionArticleModel::class);
+//
+//        if (null === ($objRow = $adapter->findByIdOrAlias($idOrAlias))) {
+//            return '';
+//        }
+//
+//        switch($insertTag) {
+//            case 'insert_theme_article':
+//                $objThemeArticle = new ModuleThemeArticle($objRow);
+//                return $objThemeArticle->generate(true);
+//                break;
+//        }
+//
+//        return '';
+//    }
+
+}
